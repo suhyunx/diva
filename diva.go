2cd3561933dd11e3b0cedffd67f3b14358ed85eb
suhyunx <53548962+suhyunx@users.noreply.github.com>
2019-08-01 12:46:05 +0900

    Update diva.go

--- diva.go
+++ diva.go
@@ -1,16 +1,12 @@
 package main
 
-
 /* Imports
  * 4 utility libraries for formatting, handling bytes, reading and writing JSON, and string manipulation
  * 2 specific Hyperledger Fabric specific libraries for Smart Contracts
  */
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
-	"strconv"
-	"time"
 
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 	sc "github.com/hyperledger/fabric/protos/peer"
@@ -25,18 +21,20 @@ type Car struct {
 	Carno   string `json:"carno"`
 	Intime  string `json:"intime"`
 	Outtime string `json:"outtime"`
-	Pay  string `json:"pay"`
+	Pay     string `json:"pay"`
 }
 
-
 //query카는 enterCar
-
-
 
 /*
  * The Invoke method is called as a result of an application request to run the Smart Contract "fabcar"
  * The calling application program has also specified the particular smart contract function to be called, with arguments
  */
+
+func (s *SmartContract) Init(APIstub shim.ChaincodeStubInterface) sc.Response {
+	return shim.Success(nil)
+}
+
 func (s *SmartContract) Invoke(APIstub shim.ChaincodeStubInterface) sc.Response {
 
 	// Retrieve the requested Smart Contract function and arguments
@@ -44,13 +42,15 @@ func (s *SmartContract) Invoke(APIstub shim.ChaincodeStubInterface) sc.Response 
 	// Route to the appropriate handler function to interact with the ledger appropriately
 	if function == "enterCar" {
 		return s.enterCar(APIstub, args)
-	}else if function == "outCar" {
+	} else if function == "outCar" {
 		return s.outCar(APIstub, args)
 	} else if function == "getInfo" {
 		return s.getInfo(APIstub, args)
-	
+	} else {
+		return shim.Error("Invalid Smart Contract function name.")
+	}
 
-	return shim.Error("Invalid Smart Contract function name.")
+	//return shim.Success(nil)
 }
 
 func (s *SmartContract) getInfo(APIstub shim.ChaincodeStubInterface, args []string) sc.Response {
@@ -63,25 +63,20 @@ func (s *SmartContract) getInfo(APIstub shim.ChaincodeStubInterface, args []stri
 	return shim.Success(carAsBytes)
 }
 
-
 func (s *SmartContract) enterCar(APIstub shim.ChaincodeStubInterface, args []string) sc.Response {
-	
+
 	if len(args) != 1 {
-	var car = Car{Carno: args[1], Intime: args[2], Outtime: args[3], Pay: args[4]}
+		return shim.Error("Incorrect number of arguments. Expecting 1")
+
 	}
 
-	start := time.Now()
-	... operation that takes 20 milliseconds ...
-	t := time.Now()
-	elapsed := t.Sub(start)
-
+	var car = Car{Carno: args[1], Intime: args[2], Outtime: args[3], Pay: args[4]}
 
 	carAsBytes, _ := json.Marshal(car)
 	APIstub.PutState(args[0], carAsBytes)
 
 	return shim.Success(nil)
 }
-
 
 func (s *SmartContract) outCar(APIstub shim.ChaincodeStubInterface, args []string) sc.Response {
 
