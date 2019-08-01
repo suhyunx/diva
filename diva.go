3dfaeff88c85f333f1d68e3822f091203e1f6e1d
suhyunx <53548962+suhyunx@users.noreply.github.com>
2019-08-01 12:31:03 +0900

    Add files via upload

---
+++ diva.go
@@ -0,0 +1,112 @@
+package main
+
+
+/* Imports
+ * 4 utility libraries for formatting, handling bytes, reading and writing JSON, and string manipulation
+ * 2 specific Hyperledger Fabric specific libraries for Smart Contracts
+ */
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"strconv"
+	"time"
+
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+	sc "github.com/hyperledger/fabric/protos/peer"
+)
+
+// Define the Smart Contract structure
+type SmartContract struct {
+}
+
+// Define the car structure, with 4 properties.  Structure tags are used by encoding/json library
+type Car struct {
+	Carno   string `json:"carno"`
+	Intime  string `json:"intime"`
+	Outtime string `json:"outtime"`
+	Pay  string `json:"pay"`
+}
+
+
+//query카는 enterCar
+
+
+
+/*
+ * The Invoke method is called as a result of an application request to run the Smart Contract "fabcar"
+ * The calling application program has also specified the particular smart contract function to be called, with arguments
+ */
+func (s *SmartContract) Invoke(APIstub shim.ChaincodeStubInterface) sc.Response {
+
+	// Retrieve the requested Smart Contract function and arguments
+	function, args := APIstub.GetFunctionAndParameters()
+	// Route to the appropriate handler function to interact with the ledger appropriately
+	if function == "enterCar" {
+		return s.enterCar(APIstub, args)
+	}else if function == "outCar" {
+		return s.outCar(APIstub, args)
+	} else if function == "getInfo" {
+		return s.getInfo(APIstub, args)
+	
+
+	return shim.Error("Invalid Smart Contract function name.")
+}
+
+func (s *SmartContract) getInfo(APIstub shim.ChaincodeStubInterface, args []string) sc.Response {
+
+	if len(args) != 1 {
+		return shim.Error("Incorrect number of arguments. Expecting 1")
+	}
+
+	carAsBytes, _ := APIstub.GetState(args[0])
+	return shim.Success(carAsBytes)
+}
+
+
+func (s *SmartContract) enterCar(APIstub shim.ChaincodeStubInterface, args []string) sc.Response {
+	
+	if len(args) != 1 {
+	var car = Car{Carno: args[1], Intime: args[2], Outtime: args[3], Pay: args[4]}
+	}
+
+	start := time.Now()
+	... operation that takes 20 milliseconds ...
+	t := time.Now()
+	elapsed := t.Sub(start)
+
+
+	carAsBytes, _ := json.Marshal(car)
+	APIstub.PutState(args[0], carAsBytes)
+
+	return shim.Success(nil)
+}
+
+
+func (s *SmartContract) outCar(APIstub shim.ChaincodeStubInterface, args []string) sc.Response {
+
+	if len(args) != 2 {
+		return shim.Error("Incorrect number of arguments. Expecting 2")
+	}
+
+	carAsBytes, _ := APIstub.GetState(args[0])
+	car := Car{}
+
+	json.Unmarshal(carAsBytes, &car)
+	car.Pay = args[1]
+
+	carAsBytes, _ = json.Marshal(car)
+	APIstub.PutState(args[0], carAsBytes)
+
+	return shim.Success(nil)
+}
+
+// The main function is only relevant in unit test mode. Only included here for completeness.
+func main() {
+
+	// Create a new Smart Contract
+	err := shim.Start(new(SmartContract))
+	if err != nil {
+		fmt.Printf("Error creating new Smart Contract: %s", err)
+	}
+}
